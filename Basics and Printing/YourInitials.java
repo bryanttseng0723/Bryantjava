6e5f8ca1071a7e00a4d7191a0d9792829f95277f
bryanttseng0723 <112799707+bryanttseng0723@users.noreply.github.com>
2022-09-05 00:12:35 +0800

    Add files via upload

---
+++ Basics and Printing/YourInitials.java
@@ -0,0 +1,14 @@
+
+public class YourInitials
+{
+    public static void main( String[] args )
+    {
+        System.out.println("BBBB   RRRR   Y   Y    A    N   N  TTTTT  ");
+        System.out.println("B   B  R   R   Y Y    A A   NN  N    T    ");
+        System.out.println("B   B  R   R    Y    A   A  N N N    T    ");
+        System.out.println("BBBB   RRRR     Y    AAAAA  N  NN    T    ");
+        System.out.println("B   B  R R      Y    A   A  N   N    T    ");
+        System.out.println("B   B  R  R     Y    A   A  N   N    T    ");
+        System.out.println("BBBB   R   R    Y    A   A  N   N    T    ");                       
+    }
+}
