e388f77ddda231e1c338649f94b305d0d2520545
bryanttseng0723 <112799707+bryanttseng0723@users.noreply.github.com>
2022-09-05 00:11:52 +0800

    Add files via upload

---
+++ Basics and Printing/LetterToYourself.java
@@ -0,0 +1,18 @@
+
+public class LetterToYourself
+{
+    public static void main( String[] args )
+    {
+        System.out.println("+----------------------------------------------------------------------+");
+        System.out.println("|                                                                 #### |");
+        System.out.println("|                                                                 #### |");
+        System.out.println("|                                                                 #### |");
+        System.out.println("|                                                                      |");
+        System.out.println("|                                                                      |");
+        System.out.println("|                                                    Bryant Tseng      |");
+        System.out.println("|                                                    1 mingsheng Rd.   |");
+        System.out.println("|                                                    Redmond, WA 98104 |");
+        System.out.println("|                                                                      |");
+        System.out.println("+----------------------------------------------------------------------+");
+    }
+}
