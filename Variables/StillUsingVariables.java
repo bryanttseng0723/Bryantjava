a19e8e1b0255f7435ccd4c6ad7ff921558d34679
bryanttseng0723 <112799707+bryanttseng0723@users.noreply.github.com>
2022-09-11 19:51:58 +0800

    Add files via upload

---
+++ Variables/StillUsingVariables.java
@@ -0,0 +1,16 @@
+public class StillUsingVariables
+{
+    public static void main( String[] args )
+    {
+        String Name;
+        int year = 2010;
+        
+
+        Name = "Juan Valdez"; 
+
+
+        System.out.println( "My name is  " + Name +"and I'll graduate in  "+ year  );
+
+
+    }
+}
