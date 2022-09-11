ea9650983eb25eb0000dceb88ddffd007ced08a6
bryanttseng0723 <112799707+bryanttseng0723@users.noreply.github.com>
2022-09-11 19:44:39 +0800

    Add files via upload

---
+++ Variables/UsingVariables.java
@@ -0,0 +1,17 @@
+public class UsingVariables
+{
+    public static void main( String[] args )
+    {
+        String Interest;
+        int room = 113;
+        double value = 2.71828;
+
+        Interest = "Computer Science"; 
+
+
+        System.out.println( "This is room # " + room  );
+        System.out.println( "e is close to" + value );
+        System.out.println( "I am learning a bit about" + Interest );
+
+    }
+}
