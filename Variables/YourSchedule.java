dfd16a64beea5d88b56255fd8053c6a2106a67a1
bryanttseng0723 <112799707+bryanttseng0723@users.noreply.github.com>
2022-09-11 20:26:45 +0800

    Add files via upload

---
+++ Variables/YourSchedule.java
@@ -0,0 +1,38 @@
+public class YourSchedule
+{
+    public static void main( String[] args )
+    {
+        String class1, class2, class3, class4, class5, class6, class7, class8, teacher1,  teacher2,  teacher3,  teacher4,  teacher5,  teacher6,  teacher7,  teacher8;
+        
+        class1 = "English III"; 
+        class2 = "psychology";
+        class3 = "physics";
+        class4 = "Technology";
+        class5 = "society";
+        class6 = "math";
+        class7 = "programming";
+        class8 = "Chemical";
+        teacher1 = "Ms. Lewis";
+        teacher2 = "Mrs. chen";
+        teacher3 = "Mrs. Yurie";
+        teacher4 = "Ms. Duke";
+        teacher5 = "Ms. English";
+        teacher6 = "Ms. Seiko";
+        teacher7 = "Ms. Jesime";
+        teacher8 = "Ms. York";
+
+
+        System.out.println( "+------------------------------------------------------------+" );
+        System.out.println( "| 1 |                          "+class1+"|       "+teacher1+"|" );
+        System.out.println( "| 2 |                          "+class2+"|       "+teacher2+"|" );
+        System.out.println( "| 3 |                          "+class3+"|       "+teacher3+"|" );
+        System.out.println( "| 4 |                          "+class4+"|       "+teacher4+"|" );
+        System.out.println( "| 5 |                          "+class5+"|       "+teacher5+"|" );
+        System.out.println( "| 6 |                          "+class6+"|       "+teacher6+"|" );
+        System.out.println( "| 7 |                          "+class7+"|       "+teacher7+"|" );
+        System.out.println( "| 8 |                          "+class8+"|       "+teacher8+"|" );
+        System.out.println( "+------------------------------------------------------------+" );
+
+
+    }
+}
