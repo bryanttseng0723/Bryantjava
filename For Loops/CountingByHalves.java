f79c226e0002ff4a78ff01ca37113bef2dcfd24c
bryanttseng0723 <112799707+bryanttseng0723@users.noreply.github.com>
2022-10-23 16:50:05 +0800

    Update CountingByHalves.java

--- For Loops/CountingByHalves.java
+++ For Loops/CountingByHalves.java
@@ -6,13 +6,7 @@ public class CountingByHalves
     {
         Scanner keyboard = new Scanner(System.in);
 
-        //System.out.println( "Type in a message, and I'll display it ten times." );
-        //System.out.print( "Count from: " );
-        //int Count = keyboard.nextInt();
-        //System.out.print( "Count to: " );
-        //int End = keyboard.nextInt();
-        //System.out.print( "Count By: " );
-        //int Gap = keyboard.nextInt();
+   
         System.out.println("x");
         System.out.println("---------------");
         
