4cfdb6960c8f007ebbd079ecf052fa8203c5fb10
bryanttseng0723 <112799707+bryanttseng0723@users.noreply.github.com>
2023-01-14 15:52:25 +0800

    Add files via upload

---
+++ Variables C++/UsingVariables.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+
+using namespace std;
+
+
+int main()
+{
+    string Interest;
+    int room = 113;
+    double value = 2.71828;
+
+    Interest = "Computer Science"; 
+
+
+    cout<<"This is room # " << room <<".\n";
+    cout<<"e is close to" << value <<".\n";
+    cout<<"I am learning a bit about " << Interest << ".\n";
+   
+}
