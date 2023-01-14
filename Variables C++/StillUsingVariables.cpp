e5956969de5dc7349ac489655be832bdb923b223
bryanttseng0723 <112799707+bryanttseng0723@users.noreply.github.com>
2023-01-14 15:56:56 +0800

    Add files via upload

---
+++ Variables C++/StillUsingVariables.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+
+using namespace std;
+
+
+int main()
+{
+    string Name;
+    int year = 2010;
+
+    Name = "Juan Valdez"; 
+
+
+    cout<<"My name is " << Name <<" and I'll graduate in "<< year<<".\n";
+    
+   
+}
