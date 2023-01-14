edf60c4082a8d36c1085a2353be456248f348161
bryanttseng0723 <112799707+bryanttseng0723@users.noreply.github.com>
2023-01-14 16:10:49 +0800

    Add files via upload

---
+++ Variables C++/YourSchedule.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+
+using namespace std;
+
+
+int main()
+{
+    string class1, class2, class3, class4, class5, class6, class7, class8, teacher1,  teacher2,  teacher3,  teacher4,  teacher5,  teacher6,  teacher7,  teacher8;
+    class1 = "English III"; 
+    class2 = "psychology";
+    class3 = "physics";
+    class4 = "Technology";
+    class5 = "society";
+    class6 = "math";
+    class7 = "programming";
+    class8 = "Chemical";
+    teacher1 = "Ms. Lewis";
+    teacher2 = "Mrs. chen";
+    teacher3 = "Mrs. Yurie";
+    teacher4 = "Ms. Duke";
+    teacher5 = "Ms. English";
+    teacher6 = "Ms. Seiko";
+    teacher7 = "Ms. Jesime";
+    teacher8 = "Ms. York";
+
+    cout<<"+------------------------------------------------------------+\n";
+    cout<<"| 1 |                           "<<class1<<"|       "<< teacher1<< " |\n";
+    cout<<"| 2 |                            "<<class2<<"|       "<< teacher2<< " |\n";
+    cout<<"| 3 |                               "<<class3<<"|      "<< teacher3<< " |\n";
+    cout<<"| 4 |                            "<<class4<<"|        "<< teacher4<< " |\n";
+    cout<<"| 5 |                               "<<class5<<"|     "<< teacher5<< " |\n";
+    cout<<"| 6 |                                  "<<class6<<"|       "<< teacher6<< " |\n";
+    cout<<"| 7 |                           "<<class7<<"|      "<< teacher7<< " |\n";
+    cout<<"| 8 |                              "<<class8<<"|        "<< teacher8<< " |\n";
+    cout<<"+------------------------------------------------------------+\n";
+    
+    
+   
+}
